cc134e03787ed2208566ecea070372a2977ff6ee
Twan H <Twan H>
2016-12-28 15:42:15 +0100

    Named URLS and changed pattern for indec

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from . import views
 
+app_name = 'main'
+
 urlpatterns = [
     # Index page
-    url(r'^index/', views.index)
+    url(r'^$', views.index, name='main_index')
 ]
