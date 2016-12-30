783e96d3db92492cb6507cd5ab133550c975338b
Twan H <Twan H>
2016-12-30 14:51:01 +0100

    Added article_index and article_detail view

--- info/views.py
+++ info/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Article
 
-# Create your views here.
+# Shows a (list) index vieuw from all the articles
+def article_index(request):
+    aricles = Article.objects.all()
+    args = {}
+    args.update(csrf(request))
+    args['articles'] = articles
+    return render(request, 'info/articles/index.html', context=args)
+
+# Show a detail view of the wanted article
+def detail(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    return render(request, 'info/articles/details.html', context={'article': article})
+
