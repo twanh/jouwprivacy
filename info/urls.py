c16473b1b5c33f28d6780589336087c7e4377235
Twan H <Twan H>
2016-12-30 14:47:37 +0100

    Added url for the aricle_index

--- info/urls.py
+++ info/urls.py
@@ -4,6 +4,10 @@ from . import views
 app_name = 'info'
 
 urlpatterns = [
+
+    # /i/aricles/
+    url(r'^articles/$', views.article_index, name='info_article_index'),
+
     # /i/article/<pk>/
     url(r'^article/(?P<pk>[0-9]+)/$', views.detail, name='info_article_detail'),
 
