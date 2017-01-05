cfd81bbdf04a711bfd440930954255e07f30b88f
Twan H <Twan H>
2017-01-05 19:24:36 +0100

    Added comments

--- info/views.py
+++ info/views.py
@@ -13,20 +13,26 @@ def article_index(request):
 def article_search(request):
     # Checks if the request method = POST so we can receive the data possted by ajax
     if  request.method == 'POST':
-        # Checks if the posted data contains a search query
+        # Checks if the posted data contains a search query and saves it
         if request.POST['query']:
             search_query = request.POST['query']
         else:
+            # If there is no post of query save an empty search query
             search_query = ''
+        # Checks if there is a category list in POST and saves it
         if request.POST.getlist('category'):
             category = request.POST.getlist('category')
         else:
+            # If there is not post of category save an empty list
             category = []
     else:
+        # If there is no post save empty variables
         search_query = ''
         category = []
 
+    # Checks if there is a , in the search query wich would indicate that the query is tag based
     if ',' in search_query:
+        # Gets all the articles from the database and compares there tags to the queries tags
         articles = Article.objects.all()
         matching = []
         qtags = search_query.replace(' ', '').split(',')
@@ -35,14 +41,17 @@ def article_search(request):
             for qtag in qtags:
                 if qtag in a_tags:
                     matching.append(article)
-
         articles = matching
     else:
+        # If the search query is not tag based the query is title based
         articles = Article.objects.filter(title__icontains=search_query)
 
+    # Checks if there is a category and filters articles on category
     if category != []:
         if category[0] != 'all':
             articles = articles.filter(category=category[0])
+
+    # Returns the ajax template with the needed articles
     return render(request, 'info/articles/ajax_search.html', {'articles': articles})
 
 
